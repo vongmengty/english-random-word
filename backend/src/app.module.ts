be8b374d1548649b63df91db27d176fde46154f6
Mengty <vongmengty55@gmail.com>
2026-06-17 14:42:07 +0700

    Remove package.json and dev script files to clean up the project structure

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { WordsController } from "./words/words.controller";
-import { WordsService } from "./words/words.service";
-
-@Module({
-  controllers: [WordsController],
-  providers: [WordsService]
-})
-export class AppModule {}
